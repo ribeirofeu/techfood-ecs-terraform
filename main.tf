2beb9e1d9bc387735086bc5bf69ed21751e92d81
Lucas Soares <lucas.soares.araujo@hotmail.com>
2023-11-04 22:23:08 +0000

    Ajusta porta do ecs

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "sg" {
   ingress {
     description = "http"
     from_port   = 80
-    to_port     = 80
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
