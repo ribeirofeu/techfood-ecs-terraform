5c011e462ff2a0afc84b8f44c2a353c9f94d1e73
Alexandre Zelante <alexandre.zelante@ibm.com>
2023-11-07 08:57:19 -0300

    comment

--- main.tf
+++ main.tf
@@ -6,259 +6,259 @@ provider "aws" {
 # VPC
 #################
 
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-}
+# resource "aws_vpc" "vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   instance_tenancy     = "default"
+#   enable_dns_hostnames = true
+# }
 
-resource "aws_subnet" "sn1" {
-  cidr_block              = "10.0.1.0/24"
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
+# resource "aws_subnet" "sn1" {
+#   cidr_block              = "10.0.1.0/24"
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "us-east-1a"
+#   map_public_ip_on_launch = true
+# }
 
-resource "aws_subnet" "sn2" {
-  cidr_block              = "10.0.2.0/24"
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-}
+# resource "aws_subnet" "sn2" {
+#   cidr_block              = "10.0.2.0/24"
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "us-east-1b"
+#   map_public_ip_on_launch = true
+# }
 
-resource "aws_subnet" "sn3" {
-  cidr_block              = "10.0.3.0/24"
-  vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-}
+# resource "aws_subnet" "sn3" {
+#   cidr_block              = "10.0.3.0/24"
+#   vpc_id                  = aws_vpc.vpc.id
+#   availability_zone       = "us-east-1c"
+#   map_public_ip_on_launch = true
+# }
 
-resource "aws_security_group" "sg" {
-  name   = "sg"
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_security_group" "sg" {
+#   name   = "sg"
+#   vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    description = "https"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "https"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description = "http"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "http"
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gw.id
-  }
-}
-
-resource "aws_route_table_association" "route1" {
-  route_table_id = aws_route_table.rt.id
-  subnet_id      = aws_subnet.sn1.id
-}
-
-resource "aws_route_table_association" "route2" {
-  route_table_id = aws_route_table.rt.id
-  subnet_id      = aws_subnet.sn2.id
-}
-
-resource "aws_route_table_association" "route3" {
-  route_table_id = aws_route_table.rt.id
-  subnet_id      = aws_subnet.sn3.id
-}
-
-################
-## CLOUDWATCH
-################
-
-resource "aws_cloudwatch_log_group" "techchallenge-logs" {
-  name              = "awslogs-techchallenge"
-  retention_in_days = 3
-}
-
-################
-## ALB
-################
-
-# Security Group para o ALB
-resource "aws_security_group" "alb_sg" {
-  name   = "alb-sg"
-  vpc_id = aws_vpc.vpc.id
-
-  ingress {
-    description = "http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "https"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
-# Application Load Balancer (ALB)
-resource "aws_lb" "app_lb" {
-  name               = "app-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.vpc.id
+# }
 
-  enable_deletion_protection = false
-}
+# resource "aws_route_table" "rt" {
+#   vpc_id = aws_vpc.vpc.id
 
-# Target Group para o ALB
-resource "aws_lb_target_group" "tg" {
-  name        = "app-tg"
-  port        = 8080
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
 
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    path                = "/actuator/health"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    interval            = 30
-    matcher             = "200"
-  }
-}
+#   route {
+#     ipv6_cidr_block = "::/0"
+#     gateway_id      = aws_internet_gateway.gw.id
+#   }
+# }
 
-# Listener para o ALB
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = "80"
-  protocol          = "HTTP"
+# resource "aws_route_table_association" "route1" {
+#   route_table_id = aws_route_table.rt.id
+#   subnet_id      = aws_subnet.sn1.id
+# }
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
+# resource "aws_route_table_association" "route2" {
+#   route_table_id = aws_route_table.rt.id
+#   subnet_id      = aws_subnet.sn2.id
+# }
 
-###############
-# ECS
-###############
+# resource "aws_route_table_association" "route3" {
+#   route_table_id = aws_route_table.rt.id
+#   subnet_id      = aws_subnet.sn3.id
+# }
 
-resource "aws_ecs_cluster" "ecs" {
-  name = "app_cluster"
-}
+# ################
+# ## CLOUDWATCH
+# ################
 
-resource "aws_ecs_service" "service" {
-  name                   = "app_service"
-  cluster                = aws_ecs_cluster.ecs.arn
-  launch_type            = "FARGATE"
-  enable_execute_command = true
+# resource "aws_cloudwatch_log_group" "techchallenge-logs" {
+#   name              = "awslogs-techchallenge"
+#   retention_in_days = 3
+# }
 
-  deployment_maximum_percent         = 200
-  deployment_minimum_healthy_percent = 100
-  desired_count                      = 1
-  task_definition                    = aws_ecs_task_definition.td.arn
+# ################
+# ## ALB
+# ################
 
-  network_configuration {
-    assign_public_ip = true
-    security_groups  = [aws_security_group.sg.id]
-    subnets          = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
-  }
+# # Security Group para o ALB
+# resource "aws_security_group" "alb_sg" {
+#   name   = "alb-sg"
+#   vpc_id = aws_vpc.vpc.id
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.tg.arn
-    container_name   = "app"
-    container_port   = 8080
-  }
-}
+#   ingress {
+#     description = "http"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-resource "aws_ecs_task_definition" "td" {
-  container_definitions = jsonencode([
-    {
-      name      = "app"
-      image     = "027260563052.dkr.ecr.us-east-1.amazonaws.com/app_repo"
-      cpu       = 512
-      memory    = 1024
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
-    }
-  ])
-  family                   = "app"
-  requires_compatibilities = ["FARGATE"]
+#   ingress {
+#     description = "https"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  cpu                = "512"
-  memory             = "1024"
-  network_mode       = "awsvpc"
-  task_role_arn      = aws_iam_role.ecs_execution_role.arn
-  execution_role_arn = aws_iam_role.ecs_execution_role.arn
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsExecutionRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+# # Application Load Balancer (ALB)
+# resource "aws_lb" "app_lb" {
+#   name               = "app-lb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb_sg.id]
+#   subnets            = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
 
-resource "aws_iam_policy_attachment" "ecs_execution_role_attachment" {
-  name       = "ecsExecutionRoleAttachment"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  roles      = [aws_iam_role.ecs_execution_role.name]
-}
+#   enable_deletion_protection = false
+# }
+
+# # Target Group para o ALB
+# resource "aws_lb_target_group" "tg" {
+#   name        = "app-tg"
+#   port        = 8080
+#   protocol    = "HTTP"
+#   target_type = "ip"
+#   vpc_id      = aws_vpc.vpc.id
+
+#   health_check {
+#     enabled             = true
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 5
+#     path                = "/actuator/health"
+#     port                = "traffic-port"
+#     protocol            = "HTTP"
+#     interval            = 30
+#     matcher             = "200"
+#   }
+# }
+
+# # Listener para o ALB
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.app_lb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tg.arn
+#   }
+# }
+
+# ###############
+# # ECS
+# ###############
+
+# resource "aws_ecs_cluster" "ecs" {
+#   name = "app_cluster"
+# }
+
+# resource "aws_ecs_service" "service" {
+#   name                   = "app_service"
+#   cluster                = aws_ecs_cluster.ecs.arn
+#   launch_type            = "FARGATE"
+#   enable_execute_command = true
+
+#   deployment_maximum_percent         = 200
+#   deployment_minimum_healthy_percent = 100
+#   desired_count                      = 1
+#   task_definition                    = aws_ecs_task_definition.td.arn
+
+#   network_configuration {
+#     assign_public_ip = true
+#     security_groups  = [aws_security_group.sg.id]
+#     subnets          = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
+#   }
+
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.tg.arn
+#     container_name   = "app"
+#     container_port   = 8080
+#   }
+# }
+
+# resource "aws_ecs_task_definition" "td" {
+#   container_definitions = jsonencode([
+#     {
+#       name      = "app"
+#       image     = "027260563052.dkr.ecr.us-east-1.amazonaws.com/app_repo"
+#       cpu       = 512
+#       memory    = 1024
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 8080
+#           hostPort      = 8080
+#         }
+#       ]
+#     }
+#   ])
+#   family                   = "app"
+#   requires_compatibilities = ["FARGATE"]
+
+#   cpu                = "512"
+#   memory             = "1024"
+#   network_mode       = "awsvpc"
+#   task_role_arn      = aws_iam_role.ecs_execution_role.arn
+#   execution_role_arn = aws_iam_role.ecs_execution_role.arn
+# }
+
+# resource "aws_iam_role" "ecs_execution_role" {
+#   name = "ecsExecutionRole"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "ecs-tasks.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_policy_attachment" "ecs_execution_role_attachment" {
+#   name       = "ecsExecutionRoleAttachment"
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#   roles      = [aws_iam_role.ecs_execution_role.name]
+# }
