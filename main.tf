9f0f56873c239e71d4348441df988258db02060f
Alexandre Zelante <alexandre.zelante@ibm.com>
2023-11-06 22:55:46 -0300

    chore: service up video

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-################
+#################
 # VPC
-################
+#################
 
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
